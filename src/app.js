321e5f17a86477cc76326beb6bfc5c23fa46c65e
Adriano11230304 <adriano.al.pereira1@gmail.com>
2022-11-14 10:41:24 -0300

    authentication

--- src/app.js
+++ src/app.js
@@ -2,11 +2,19 @@ const express = require('express');
 const path = require('path');
 const postRouter = require('./routes/postsRoutes');
 const userRouter = require('./routes/usersRoutes');
+const session = require('express-session');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(session({
+    secret: 'ssasvdfdbbdfvdvdfv',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }
+}))
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,8 +3,16 @@ const { User } = require('../models/User');
 class UserController{
     async userAuth(req, res) {
         const users = await User.findAll();
-        console.log(users);
-        res.end('teste');
+        users.forEach(user => {
+            if(user.cpf == req.body.cpf && user.password == req.body.password){
+                req.session.user = user;
+                console.log(req.session.user);
+                res.end('Usuário logado!');
+            }else{
+                console.log(req.session.user);
+                res.end('Usuário não cadastrado. Você precisa se cadastrar!');
+            }
+        });
     }
 }
 
